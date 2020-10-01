78b34d16535709fca4d27f014de78a2cabfb6f10
bachtran2000 <45133811+bachtran2000@users.noreply.github.com>
2020-10-02 05:20:58 +0700

    30/09/2020

---
+++ TH1/src/com/company/Student.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Student {
+    private String hoten;
+    private int tuoi;
+
+    public Student() {
+    }
+
+    public Student(String hoten, int tuoi) {
+        this.hoten = hoten;
+        this.tuoi = tuoi;
+    }
+
+    public String getHoten() {
+        return hoten;
+    }
+
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ho ten:");
+        hoten = sc.nextLine();
+        System.out.print("Nhap nam sinh:");
+        tuoi = sc.nextInt();
+    }
+}
---
+++ TH1/src/com/company/bai1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class bai1 {
+
+    public static void main(String[] args) {
+
+        processStudent ps = new processStudent();
+        ps.quanly();
+
+
+    }
+}
